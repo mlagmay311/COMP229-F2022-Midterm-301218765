0d00816c3ec154e694e15fa2365fa4644c789dc7
mlagmay311 <mlagmay@my.centennialcollege.ca>
2022-10-27 00:30:07 -0700

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //URI: "mongodb://127.0.0.1/Emp",
+  URI:"mongodb+srv://ekomahmen:ekomahmen@cluster0.fh2hwzf.mongodb.net/Emp?retryWrites=true&w=majority"
+};
---
+++ server/routes/employees.js
@@ -0,0 +1,101 @@
+// modules required for routing
+const { response } = require("express");
+let express = require("express");
+let router = express.Router();
+let mongoose = require("mongoose");
+
+// define the employee model
+let employee = require("../models/employees");
+
+/* GET employee List page. READ */
+router.get("/", (req, res, next) => {
+  // find all employee in the employee_detail collection
+  employee.find((err, employees) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      res.render("employees/index", {
+        title: "Employees",
+        employees: employees,
+      });
+    }
+  });
+});
+
+//  GET the Employee Details page in order to add a new employee
+router.get("/add", (req, res, next) => {
+res.render("employees/add", {title: "Add Employee"});
+  
+});
+
+// POST process the Employee Details page and create a new Employee - CREATE
+router.post("/add", (req, res, next) => {
+  let newEmployee = employee({
+      Employeeid: req.body.Employeeid,
+      Employeename: req.body.Employeename,
+      Department: req.body.Department,
+      Designation: req.body.Designation,
+      Salary: req.body.Salary,
+  });
+  employee.create(newEmployee, (err, employee)=>{
+      if(err){
+        console.log(err);
+        res.end(err);
+      } else {
+        res.redirect("/employees");
+      }
+  });
+
+});
+
+// GET the Employee Details page in order to edit an existing Employee
+router.get("/details/:id", (req, res, next) => {
+    let id = req.params.id;
+    employee.findById(id,(err, employeetoedit)=>{
+      if (err){
+        console.log(err);
+        res.end(err);
+      }else {
+        res.render("employees/details",{title: "edit employee",employee:employeetoedit});
+      }
+    });
+});
+
+// POST - process the information passed from the details form and update the document
+router.post("/details/:id", (req, res, next) => {
+  let id = req.params.id;
+  let updateemployee = employee({
+    _id: id,
+    Employeeid: req.body.Employeeid,
+    Employeename: req.body.Employeename,
+    Department: req.body.Department,
+    Designation: req.body.Designation,
+    Salary: req.body.Salary,
+  });
+  employee.updateOne({_id:id},updateemployee,(err)=>{
+    if(err){
+      console.log(err);
+      res.end(err);
+    } else {
+      res.redirect("/employees");
+    }
+  });
+});
+
+// GET - process the delete by specific employeename
+router.get("/delete/:Employeename", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+   let Employeename = req.params.Employeename;
+   employee.remove({ Employeename: Employeename }, (err) => {
+     if (err) {
+       console.log(err);
+       res.end(err);
+     } else {
+       res.redirect("/employees");
+     }
+   });
+});
+
+module.exports = router;
